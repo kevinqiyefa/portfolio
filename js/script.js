40f4b53170f0a94b339b6f499327560b8f94ec6e
Kevin <kevinqiyf@hotmail.com>
2020-12-03 18:55:25 -0800

    update resume and skills

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 var i = 0;
 var txt =
-  'Computer Science graduate and full stack software engineer proficient in React, JavaScript, and Express.';
+	'Front-end focused full-stack software engineer and proficient in Javascript and React.';
 var speed = 50;
 
 window.onload = function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById('type').innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
-  var d = new Date();
-  var year = d.getFullYear();
-  let text = `© ${year}  Kevin Qi | All rights reserved.`;
-  document.getElementById('footerText').innerHTML = text;
+	if (i < txt.length) {
+		document.getElementById('type').innerHTML += txt.charAt(i);
+		i++;
+		setTimeout(typeWriter, speed);
+	}
+	var d = new Date();
+	var year = d.getFullYear();
+	let text = `© ${year}  Kevin Qi | All rights reserved.`;
+	document.getElementById('footerText').innerHTML = text;
 };
